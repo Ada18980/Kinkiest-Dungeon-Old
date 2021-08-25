eac1e1fd6c9407ceb477ce73e82a2d7f40ffcac4
Ada18980 <gubertpol0@gmail.com>
2021-08-25 15:00:13 -0700

    Viewport

--- src/launcher.ts
+++ src/launcher.ts
@@ -1,46 +1,61 @@
 import * as PIXI from 'pixi.js';
 import { AbstractRenderer } from 'pixi.js';
+import { Viewport } from 'pixi-viewport'
 
 let app: PIXI.Application;
+let renderer: PIXI.Renderer;
+let viewport: Viewport;
 let windowSize = {width: 1920, height: 1080};
 let ratio = windowSize.width / windowSize.height;
 
+const TILE_SIZE = 64;
+const MIN_ZOOM = 7; // In tiles
+const MAX_ZOOM = 25; // In Tiles
+
 export function LauncherLaunchGame(width: number, height: number): void {
-	windowSize.width = width;
-	windowSize.height = height;
-	ratio = windowSize.width / windowSize.height;
+	setWindowSize(width, height);
 
-	function resize() {
-		let zoom = (( window.outerWidth) / window.innerWidth);
-
-		if (window.innerWidth / window.innerHeight >= ratio) {
-			var w = window.innerHeight * ratio * zoom;
-			var h = window.innerHeight * zoom;
-		} else {
-			var w = window.innerWidth * zoom;
-			var h = window.innerWidth / ratio * zoom;
-		}
-		renderer.view.style.width = w + 'px';
-		renderer.view.style.height = h + 'px';
-
-		//renderer.view.style.position = "relative";
-		//renderer.view.style.left = ((window.innerWidth - w) >> 1) + 'px'
-		//renderer.view.style.top = ((window.innerHeight - h) >> 1) + 'px';
-
-	}
 	window.onresize = function(event) {
 		resize();
 	};
 
-
-
 	// Creates the document
-	app = new PIXI.Application({ width, height,
+	app = new PIXI.Application({
+		resizeTo: window,
 		autoDensity: true,
 		backgroundColor: 0x1099bb,
 		antialias: false,
 	});
+
+	renderer = app.renderer as PIXI.Renderer;
 	app.view.setAttribute("id", "mainCanvas");
+
+	// create viewport
+	viewport = new Viewport({
+		screenWidth: width,
+		screenHeight: height,
+
+		worldWidth: 2048,
+		worldHeight: 2048,
+
+		disableOnContextMenu: true,
+
+		interaction: app.renderer.plugins.interaction // the interaction module is important for wheel to work properly when renderer.view is placed or scaled
+	});
+
+	// add the viewport to the stage
+	app.stage.addChild(viewport);
+	// activate plugins
+	viewport
+		.drag({mouseButtons: "left"})
+		.pinch()
+		.wheel({smooth: 5})
+		.decelerate({friction: .9})
+		.clamp({direction: "all"})
+		.clampZoom(clampZoomOptions());
+
+
+
 
 	PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
 
@@ -53,34 +68,101 @@ export function LauncherLaunchGame(width: number, height: number): void {
 
 
 	let stage = app.stage;
-	let renderer = app.renderer;
 	document.body.appendChild(renderer.view);
 
-	let texture = PIXI.RenderTexture.create({ width: 300, height: 300 });
-	let r1 = new PIXI.Graphics();
-	r1.beginFill(0x000000);
-	r1.drawRect(100, 100, 100, 100);
-	r1.endFill();
-	renderer.render(r1,{renderTexture: texture})
-	var block = new PIXI.Sprite(texture);
-	block.position.x = 200;
-	block.position.y = 200;
-	block.anchor.x = .5;
-	block.anchor.y = .5;
-	stage.addChild(block);
+	for (let i = 0; i < 2048/128; i += 1) {
+		for (let ii = 0; ii <= 2048/64; ii += 1) {
+			let texture = PIXI.RenderTexture.create({ width: 64, height: 64 });
+			let r1 = new PIXI.Graphics();
+			r1.beginFill(0x000000);
+			r1.drawRect(0, 0, 64, 64);
+			r1.endFill();
+			renderer.render(r1,{renderTexture: texture})
+			let block = new PIXI.Sprite(texture);
+			block.position.x = 128*i + (ii % 2 == 0 ? 0 : 64);
+			block.position.y = 64*ii;
+			block.anchor.x = 0;
+			block.anchor.y = 0;
+			viewport.addChild(block);
+		}
+	}
+
 	resize();
 
 	// Listen for animate update
 	var lastTick = performance.now();
+
+	viewport.addListener("clicked", (event) => {viewport.snap(event.world.x, event.world.y, {ease: "easeInOutSine", time: 1000, removeOnComplete: true, removeOnInterrupt: true});})
+
 	app.ticker.add((delta: number) => {
 		let d = performance.now() - lastTick;
 		lastTick = performance.now();
 
 
-		function animate() {
-			block.rotation += .01;
-		}
-		animate();
 	});
 }
 
+function resize() {
+
+	renderer.view.style.position = "absolute";
+	renderer.view.style.left = 0 + 'px'
+	renderer.view.style.top = 0 + 'px';
+
+	setWindowSize(window.innerWidth, window.innerHeight);
+
+	/*
+	if (window.innerWidth / window.innerHeight >= ratio) {
+		var w = window.innerHeight * ratio;
+		var h = window.innerHeight;
+	} else {
+		var w = window.innerWidth;
+		var h = window.innerWidth / ratio;
+	}
+	renderer.view.style.width = w + 'px';
+	renderer.view.style.height = h + 'px';
+
+	renderer.view.style.position = "absolute";
+	renderer.view.style.left = ((window.innerWidth - w) >> 1) + 'px'
+	renderer.view.style.top = ((window.innerHeight - h) >> 1) + 'px';
+	*/
+
+	viewport.screenWidth = window.innerWidth;
+	viewport.screenHeight = window.innerHeight;
+
+	viewport.clampZoom(clampZoomOptions());
+
+
+	viewport.snap(viewport.center.x+1, viewport.center.y, {time: 0, removeOnComplete: true});
+	viewport.snap(viewport.center.x-1, viewport.center.y, {time: 0, removeOnComplete: true});
+
+	//viewport.drag();
+
+}
+
+function clampZoomOptions() {
+
+	console.log({
+		minWidth: MIN_ZOOM * TILE_SIZE,
+		minHeight: MIN_ZOOM * TILE_SIZE,
+		maxWidth: MAX_ZOOM * TILE_SIZE * Math.max(1, ratio),
+		maxHeight: MAX_ZOOM * TILE_SIZE / Math.min(1, ratio),
+	});
+	return {
+		minWidth: MIN_ZOOM * TILE_SIZE,
+		minHeight: MIN_ZOOM * TILE_SIZE,
+		maxWidth: MAX_ZOOM * TILE_SIZE * Math.max(1, ratio),
+		maxHeight: MAX_ZOOM * TILE_SIZE / Math.min(1, ratio),
+	};
+}
+
+function setWindowSize(width: number, height: number) {
+	windowSize.width = width;
+	windowSize.height = height;
+	ratio = windowSize.width / windowSize.height;
+	if (ratio < 0.01) ratio = 0.01;
+}
+
+export function changeResolution(width: number, height: number) {
+	setWindowSize(width, height);
+	viewport.clampZoom(clampZoomOptions());
+}
