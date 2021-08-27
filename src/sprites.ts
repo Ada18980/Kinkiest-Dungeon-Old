6a685bb490c99eafc3093f14d114704fec011d7e
Ada18980 <gubertpol0@gmail.com>
2021-08-26 22:04:04 -0700

    Rendering

--- src/sprites.ts
+++ src/sprites.ts
@@ -1,3 +1,4 @@
+import { Viewport } from 'pixi-viewport';
 import * as PIXI from 'pixi.js';
 import { app, viewport } from './launcher';
 
@@ -51,15 +52,58 @@ interface SpriteLayer {
 }
 
 export class Image {
+    // Contains animations and layers
+    animations : Map<string, Map<string, KDSprite>>;
+    currentAnimation : string = "";
+
+    constructor() {
+        this.animations = new Map<string, Map<string, KDSprite>>();
+    }
+
+    addSprite(layer : string, animation : string, sprite : KDSprite) {
+        let anim : Map<string, KDSprite> = this.animations.get(animation) || new Map<string, KDSprite>();
+        if (!this.animations.has(animation)) this.animations.set(animation, anim);
+        anim.set(layer, sprite);
+    }
+
+    render(viewport : Viewport, animation : string, x : number, y : number, rotation : number = 0) {
+        let currRender = (animation != this.currentAnimation) ? this.animations.get(this.currentAnimation) : null;
+        let toRender = this.animations.get(animation);
+
+        if (currRender) {
+            currRender.forEach((element) => {
+                element.sprite.visible = false;
+            });
+        }
+
+        if (toRender) {
+            toRender.forEach((element) => {
+                element.sprite.visible = true;
+                element.sprite.position.set(x, y);
+                element.sprite.rotation = rotation;
+
+                if (element.viewport != viewport) {
+                    if (element.viewport) element.viewport.removeChild(element.sprite);
+                    viewport.addChild(element.sprite);
+                    element.viewport = viewport;
+                }
+            });
+
+            this.currentAnimation = animation;
+        }
+    }
+}
+
+export class KDSprite {
     sprite : PIXI.Sprite;
-    columns : number = 1;
-    width : number = 50;
-    height : number = 64;
-    constructor(sprite : PIXI.Sprite, columns : number = 1, width : number = 50, height : number = 64) {
+    frames : number;
+    noLoop : boolean;
+    viewport : Viewport | null = null;
+
+    constructor(sprite : PIXI.Sprite, frames : number = 1, noLoop : boolean = false) {
         this.sprite = sprite;
-        this.columns = columns;
-        this.width = width;
-        this.height = height;
+        this.frames = frames;
+        this.noLoop = noLoop;
     }
 }
 
@@ -84,33 +128,52 @@ export function loadSprites() {
     PIXI.Loader.shared.load((loader, resources) => {
         spriteResources.forEach((element) => {
             let resource = resources[element.name];
+            let image = new Image();
             if (typeof resource !== "undefined") {
                 let loader = PIXI.Loader.shared.resources[element.name];
-                let animation = "walkdown";
-                let layer = "hair";
-                let frameData = loader?.spritesheet?.textures;
-                if (frameData) {
-                    let keys = Object.keys(frameData);
-                    let frameKeys = keys.filter((frame : string) => {
-                        return (frameData && frameData[frame] && frame.includes(`(${layer})`) && loader?.data.meta.frameTags?.some((tag: any) => {
-                                if (tag.name != animation) return false;
-                                let indexStr = frame.split(" ")[2]?.split(".")[0];
-                                if (!indexStr) return false;
-                                let index = parseInt(indexStr);
-                                console.log(tag.from);
-                                console.log(tag.to);
-                                console.log(tag.from != null && tag.to != null && index >= tag.from && index <= tag.to);
-                                return tag.from != null && tag.to != null && index >= tag.from && index <= tag.to;
-                            })
-                        );})
-                    let frames = frameKeys.map((frame: string) => {return (frameData && frameData[frame]) || nullTexture;});
-                    console.log(frameData);
-                    console.log(keys);
-                    console.log(frameKeys);
-                    console.log(frames);
-                    if (frames)
-                        sprites.set(element.name, new Image(new PIXI.AnimatedSprite(frames)));
-                }
+
+                let anims : string[] = [];
+                let layers : string[] = [];
+
+                // Seed animations and layers from metatada
+                if (loader?.data?.meta?.frameTags) loader?.data?.meta?.frameTags.forEach((tag : any) => {
+                    if (tag.name) anims.push(tag.name);
+                })
+                if (loader?.data?.meta?.layers) loader?.data?.meta?.layers.forEach((layer : any) => {
+                    if (layer.name) layers.push(layer.name);
+                })
+
+                // Default layer and animation
+                if (anims.length == 0) anims.push("idle");
+                if (layers.length == 0) layers.push("base");
+
+                // Load the sprites in the internal format
+                anims.forEach((animation : string) => {
+                    layers.forEach((layer : string) => {
+                        let frameData = loader?.spritesheet?.textures;
+                        if (frameData) {
+                            let keys = Object.keys(frameData);
+                            let frameKeys = keys.filter((frame : string) => {
+                                return (frameData && frameData[frame] && frame.includes(`(${layer})`) && loader?.data.meta.frameTags?.some((tag: any) => {
+                                        if (tag.name != animation) return false;
+                                        let indexStr = frame.split(" ")[2]?.split(".")[0];
+                                        if (!indexStr) return false;
+                                        let index = parseInt(indexStr);
+                                        return tag.from != null && tag.to != null && index >= tag.from && index <= tag.to;
+                                    })
+                                );})
+                            let frames = frameKeys.map((frame: string) => {return (frameData && frameData[frame]) || nullTexture;});
+                            if (frames) {
+                                let sprite = new PIXI.AnimatedSprite(frames);
+                                //sprite.anchor.set(0.5);
+
+                                image.addSprite(layer, animation, new KDSprite(sprite, frames.length, loader?.data?.meta?.frameTags[animation]?.noLoop));
+                            }
+                        }
+                    });
+                });
+
+                sprites.set(element.name, image);
             }
         })
     });
@@ -120,9 +183,7 @@ export function loadSprites() {
     PIXI.Loader.shared.onComplete.add(() => {
         let sprite = getSprite("player_mage");
         if (sprite) {
-            sprite.sprite.anchor.set(0.5);
-            sprite.sprite.position.set(1024,1024);
-            viewport.addChild(sprite.sprite);
+            sprite.render(viewport, "walkdown", 1024, 1024);
         }
     }); // called once when the queued resources all load.
 
